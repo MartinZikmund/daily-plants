3d3fce0e446d67303e899ca9a3ecdccabab19a03
Martin Zikmund <martin@zikmund.dev>
2026-03-16 11:36:51 +0100

    feat: Navigation view header

--- src/DailyPlants/Views/ShellView.xaml.cs
+++ src/DailyPlants/Views/ShellView.xaml.cs
@@ -86,6 +86,7 @@ public sealed partial class ShellView : Page
     {
         if (args.SelectedItem is NavigationViewItem item)
         {
+            NavView.Header = item.Content;
             var tag = item.Tag?.ToString();
             NavigateToPage(tag);
 
